e5ef941c22e5d71185fca27b3beff19cc77dda6a
Spencer Fulgham <spencerman@comcast.net>
2022-02-01 16:30:36 -0800

    new comp

--- lml-project/src/App.js
+++ lml-project/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+// import './App.css';
+
+import Head from './header.js'
 
 function App() {
+
+  // react js goes here
+
   return (
+    // html goes here
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <Head/>
+
     </div>
   );
 }
---
+++ lml-project/src/header.js
@@ -0,0 +1,11 @@
+
+function header(){
+
+    // js
+
+    return(
+        <h1> This is from header.js : Hello LML </h1>
+    )
+}
+
+export default header;
