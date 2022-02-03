1a303c9f8ee3c96f6b94de2a594b9c03c1a061e0
Spencer Fulgham <spencerman@comcast.net>
2022-02-02 19:38:21 -0800

    finished tutorial

--- lml-project/src/Tictactoe.js
+++ lml-project/src/Tictactoe.js
@@ -4,11 +4,12 @@ import '../src/App.css'
 import Board from './board.js'
 
 class Game extends React.Component {
-    render() {
+  
+    render() {      
       return (
         <div className="game">
           <div className="game-board">
-            <Board/>
+            <Board calculateWinner={calculateWinner}/>
           </div>
           <div className="game-info">
             <div>{/* status */}</div>
@@ -18,6 +19,27 @@ class Game extends React.Component {
       );
     }
 }
-  
- export default Game
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
+export default Game
   
--- lml-project/src/board.js
+++ lml-project/src/board.js
@@ -35,7 +35,10 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O'  );
+        const winner = this.props.calculateWinner(this.state.squares);
+        let status
+        if (winner) { status = 'Winner: ' + winner; }
+        else { status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O'); }
 
         return (
         <div>
