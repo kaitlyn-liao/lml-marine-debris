2408589a1f0a0be8dd494f0d762d208d5a29401b
kaitlyn-liao <59896595+kaitlyn-liao@users.noreply.github.com>
2022-02-23 14:29:12 -0800

    separated call to action and methodology page

---
+++ lml-project/src/components/landing_page/CallToAction.js
@@ -0,0 +1,32 @@
+// CallToAction.js renders the CallToAction Page on the website
+// This page displays information about how the general public can help 
+// decrease their impact on marine debris.  
+// Most of this page will be static formated text.
+
+// CallToAction.js is rendered by Controller.js, and currently renders no children.
+
+import React from 'react';
+
+class CallToAction extends React.Component {
+    
+    render() {     
+      return (
+        // html goes here
+        <div>
+        
+        <div>
+          images go here
+        </div>
+        <div>
+          text goes here
+        </div>
+        <div>
+          call to action goes here
+        </div>
+        
+        </div>
+      );
+    }
+}
+
+export default CallToAction
---
+++ lml-project/src/components/method_page/Methodology.js
@@ -0,0 +1,29 @@
+// Methodology.js renders the Methodology Page on the website
+// This page displays inforation about any methodologies for gathering data,
+// where the data was gathered from. 
+// Most of this page will be static formated text.
+
+// Methodology.js is rendered by Controller.js, and currently renders no children.
+
+import React from 'react';
+
+class Methodology extends React.Component {
+    
+    render() {     
+      return (
+        // html goes here
+        <div>
+        
+        <div>
+          images go here
+        </div>
+        <div>
+          methodology goes here
+        </div>
+        
+        </div>
+      );
+    }
+}
+
+export default Methodology
