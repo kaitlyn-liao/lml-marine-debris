904e2def21186bdaedd65553d9e08502d7561f2e
Spencer Fulgham <spencerman@comcast.net>
2022-02-23 23:45:14 -0800

    parse through local csv into json format

--- lml-project/src/components/upload_csv.js
+++ lml-project/src/components/upload_csv.js
@@ -1,18 +1,34 @@
-import React from 'react';
-
+// A file used to test the implimentation of uploading a csv file into the PostgreSQL Database
+import React, {useState} from 'react';
 import { usePapaParse } from 'react-papaparse';
+import localCSV from '../SSDS_tester.csv'
 
-export default function ReadString() {
-  const { readString } = usePapaParse();
 
-  const handleReadString = () => {
-    const csvString = `Column 1,Column 2,Column 3,Column 4
-1-1,1-2,1-3,1-4
-2-1,2-2,2-3,2-4
-3-1,3-2,3-3,3-4
-4,5,6,7`;
+// Completes the process of accepting a user's CSV file, parsing through
+// the file, and beginning the process of handing off the information to the postgreSQL
+function UploadCSV() {
+  const [uploadFile, setUploadFile] = useState([]);
+  // const [fileText, setText ] = useState('');
+  const {readString} = usePapaParse();
 
-    readString(csvString, {
+  // takes accepted file and prints to confirm up 
+  const acceptFile = (event) => {
+    event.preventDefault();
+    console.log("uploadFile", uploadFile[0]);
+  };
+
+  // loadFile() -> fetches the text content of a csv file
+  async function loadFile (){
+    return await fetch( localCSV ).then( response => response.text());
+    // fetch( localCSV ).then( response => response.text().then( responseText => setText(responseText)));
+  };
+
+  // handleReadString() -> parses through CSV text content and converts it to JSON format vis Papaparse
+  async function handleReadString() {
+  //const fileContent = `Column 1,Column 2,Column 3,Column 4' etc
+    const fileContent =  await loadFile();
+
+    readString(fileContent, {
       worker: true,
       complete: (results) => {
         console.log('---------------------------');
@@ -20,77 +36,88 @@ export default function ReadString() {
         console.log('---------------------------');
       },
     });
-  };
 
-  return <button onClick={() => handleReadString()}>readString</button>;
+    // begin to parse through and upload new entries to the lml-data datatable in PostgreSQL
+    // fileContent
+  }
+  
+  // function getMerchant() {
+  //   fetch('http://localhost:3001')
+  //     .then(response => response.json())
+  //     .then(data => { setMerchants(data);});
+  // }
+  // function createMerchant() {
+  //   let name = prompt('Enter merchant name');
+  //   let email = prompt('Enter merchant email');
+  //   fetch('http://localhost:3001/merchants', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({name, email}),
+  //   })
+  //     .then(response => {
+  //       return response.text();
+  //     })
+  //     .then(data => {
+  //       alert(data);
+  //       getMerchant();
+  //     });
+  // }
+  // function deleteMerchant() {
+  //   let id = prompt('Enter merchant id');
+  //   fetch(`http://localhost:3001/merchants/${id}`, {
+  //     method: 'DELETE',
+  //   })
+  //     .then(response => {
+  //       return response.text();
+  //     })
+  //     .then(data => {
+  //       alert(data);
+  //       getMerchant();
+  //     });
+  // }
+
+  // const showFile = async (e) => {
+  //   e.preventDefault()
+  //   const reader = new FileReader()
+  //   reader.onload = async (e) => { 
+  //     const text = (e.target.result)
+  //     console.log(text)
+  //     alert(text)
+  //   };
+  //   reader.readAsText(e.target.files[0])
+  // }
+
+  return (
+    <div>
+
+      <button onClick={handleReadString}>readString</button>
+      <br/><br/>
+
+      <form onSubmit={acceptFile}>
+        <input  className="csv-input" 
+                type="file" 
+                id="react-csv-reader-input" 
+                accept=".csv, text/csv" 
+                name="myFile" 
+                onChange={(e) => setUploadFile(e.target.files)} >    
+        </input>
+        <br/>
+        <input type="submit" /> 
+      </form>
+
+      {/* <div>
+        <button onClick={ loadFile }>loadFile</button>
+        <h2>Content:</h2>
+        <pre>{ fileText }</pre>
+      </div> */}
+    </div>
+  );
 }
 
 
-// // A file used to test the implimentation of uploading a csv file into the PostgreSQL Database
-
-// import React, {useState} from 'react';
-// import Papa from 'papaparse';
-// import localCSV from '../SSDS_tester.csv'
-
-
-// // Completes the process of accepting a user's CSV file, parsing through
-// // the file, and beginning the process of handing off the information to the postgreSQL
-// function UploadCSV() {
-//   const [uploadFile, setUploadFile] = useState([]);
-//   const [ text, setText ] = useState('');
-
-//   // takes accepted file and prints to confirm up 
-//   const acceptFile = (event) => {
-//     event.preventDefault();
-//     console.log("uploadFile", uploadFile[0]);
-//   };
-
-//   // Sets 'text' to the contents of a localCSV file
-//   const loadFile = function(){
-//     fetch( localCSV )
-//         .then( response => response.text() )
-//         .then( responseText => {
-//             setText( responseText );
-//         })
-//   };
-
-//   const showFile = async (e) => {
-//     e.preventDefault()
-//     const reader = new FileReader()
-//     reader.onload = async (e) => { 
-//       const text = (e.target.result)
-//       console.log(text)
-//       alert(text)
-//     };
-//     reader.readAsText(e.target.files[0])
-//   }
-
-//   return (
-//     <div>
-//       <form onSubmit={acceptFile}>
-//         <input  className="csv-input" 
-//                 type="file" 
-//                 id="react-csv-reader-input" 
-//                 accept=".csv, text/csv" 
-//                 name="myFile" 
-//                 onChange={(e) => setUploadFile(e.target.files)} >    
-//         </input>
-//         <br/>
-//         <input type="submit" /> 
-//       </form>
-
-//       <div>
-//         <button onClick={ loadFile }>load</button>
-//         <h2>text:</h2>
-//         <pre>{ text }</pre>
-//       </div>
-
-//     </div>
-//   );
-// }
-
-
-// export default UploadCSV
+export default UploadCSV
 
 // SQL Initialization for associated data tables
 
