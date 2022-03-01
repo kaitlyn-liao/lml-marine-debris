9078e8ee5382dc279e0ad4584e65b1fa14c4b3cc
kaitlyn-liao <59896595+kaitlyn-liao@users.noreply.github.com>
2022-02-28 16:58:11 -0800

    installed libraries for login page

--- lml-project/src/components/login_page/Login_UnApr.js
+++ lml-project/src/components/login_page/Login_UnApr.js
@@ -6,17 +6,35 @@
 // Login_UnApr.js is rendered by Login.js, and renders no children.
 
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../../css/LoginStyle.css';
 
-class Login_UnApr extends React.Component {
-    
-    render() {     
-      return (
-        // html goes here
-        <div>
+function Login_UnApr() {
 
-        </div>
-      );
-    }
+  let navigate = useNavigate();
+  const handleLogin = (event) =>{
+    console.log(event.target.value)
+    let path = `postSlug`;
+    navigate(path);
+  }
+      
+  return (
+    <div className="Login_UnApr">
+        <h1>Login</h1>
+        <form onSubmit={handleLogin}>
+            <input type="email" className="bg-gray email-input-size" name="email" type="email" placeholder=" Email" />
+            <br></br>
+            <br></br>
+
+            <input type="password" className="bg-gray email-input-size" name="password" type="password" placeholder=" Password" />
+            <br></br>
+
+            <br></br>
+            <button type="submit" className="btn-blue btn" onClick={handleLogin}>Login</button>
+        </form>
+    </div>
+  );
+
 }
 
 export default Login_UnApr
