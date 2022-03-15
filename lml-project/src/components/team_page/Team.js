71157ff87fd91e043476f18d86f8a72fcc809c2e
kaitlyn-liao <59896595+kaitlyn-liao@users.noreply.github.com>
2022-03-14 17:44:04 -0700

    fixed adblock issue and added some team images

--- lml-project/src/components/team_page/Team.js
+++ lml-project/src/components/team_page/Team.js
@@ -8,6 +8,14 @@ import React from 'react';
 import avatar from '../team_page/images/default-avatar.png'
 import githubButton from '../team_page/images/GitHub-Mark.png'
 import linkedInButton from '../team_page/images/linkedin.png'
+import juliImg from '../team_page/images/juli-limon.jpg'
+import maiaImg from '../team_page/images/maia-smith.jpeg'
+import robinImg from '../team_page/images/robin-dunkin.jpg'
+import kaitlynImg from '../team_page/images/kaitlyn-liao.JPG'
+// import bridgetImg from '../team_page/images/bridget-chew.jpg'
+// import noahImg from '../team_page/images/noah-cantwell.jpg'
+// import zackImg from '../team_page/images/zack-miller.jpg'
+import spencerImg from '../team_page/images/spencer-fulgham.png'
 
 /* When adding this file to the topology, make sure to transfer all images
     and the CSS properties from Team.css */
@@ -56,10 +64,10 @@ function Member(props) {
 *  Inputs: name, bio, photo
 *  Outputs: formatted card for LML team member with given info
 */
-function Sponsor(props) {
+function Researcher(props) {
     return (
         <div>
-            <div class="sponsor-box">
+            <div class="researcher-box">
                 <div class="row">
                     <div class="col-sm">
                         <div class="row">
@@ -90,7 +98,7 @@ class Team extends React.Component {
                 
                 <div class="row">
                     <Member name='Kaitlyn Liao' bio='Product Owner and Software Engineer, Computer Science student at UCSC' github="https://github.com/kaitlyn-liao/lml-marine-debris"
-                            photo={avatar} linkedIn="https://www.linkedin.com/"/>
+                            photo={kaitlynImg} linkedIn="https://www.linkedin.com/"/>
                 </div>
                     
                 <div class="row">
@@ -103,7 +111,7 @@ class Team extends React.Component {
                 </div>
                 <div class="row">
                     <Member name='Spencer Fulgham' bio='Developer and Software Engineer, Computer Science student at UCSC' github="https://github.com/kaitlyn-liao/lml-marine-debris"
-                        photo={avatar} linkedIn="https://www.linkedin.com/"/>
+                        photo={spencerImg} linkedIn="https://www.linkedin.com/"/>
                 </div>
                 <div class="row">
                     <Member name='Zachary Miller' bio='Developer and Software Engineer, Computer Science student at UCSC' github="https://github.com/kaitlyn-liao/lml-marine-debris"
@@ -121,7 +129,7 @@ class Team extends React.Component {
                 <p>From the UCSC Long Marine Lab.</p></div>
                 <p><br></br></p>
                 <div class="row">
-                    <Sponsor name='Robin Dunkin' bio='Dr. Robin Dunkin is the Marine Mammal Stranding Operations 
+                    <Researcher name='Robin Dunkin' bio='Dr. Robin Dunkin is the Marine Mammal Stranding Operations 
                     manager for The Long Marine Lab Stranding Network and has worked in this role since 2005. 
                     Robin completed her Ph.D. in the lab of Dr. Terrie Williams in 2012 but first began learning 
                     about marine mammal stranding response while completing her master’s degree in the lab of 
@@ -130,28 +138,28 @@ class Team extends React.Component {
                     worked for the Marine Mammal Physiology Project as an animal trainer from 1998 to 2001. Robin 
                     is responsible for the day to day operations of the stranding network and works with the stranding 
                     coordinator and the director to coordinate stranding response for Santa Cruz County.'
-                        photo={avatar} />
+                        photo={robinImg} />
                 </div>
                 <div class="row">
-                    <Sponsor name='Juliana Limon' bio='Juliana Limon is a Stranding Technician for the Long Marine Lab Stranding 
+                    <Researcher name='Juliana Limon' bio='Juliana Limon is a Stranding Technician for the Long Marine Lab Stranding 
                     Network. She is a recent graduate with a major in Marine Biology at UCSC. She began volunteering 
                     for the Long Marine Lab stranding network shortly after she transferred to UCSC in 2019 and got 
                     hired as a Stranding Technician in 2021. During her time as an undergrad she participated in 
                     research at UCSC, California Academy of Sciences, and the Monterey Bay Aquarium Research Institute. 
                     She has plans to pursue graduate school in the near future with the hopes to enter the field of 
                     conservation genomics. '
-                        photo={avatar} />
+                        photo={juliImg} />
                 </div>
                 <div class="row">
-                    <Sponsor name='Maia Smith' bio='Maia Smith is a Stranding Technician for the Long Marine Lab Stranding Network. 
+                    <Researcher name='Maia Smith' bio='Maia Smith is a Stranding Technician for the Long Marine Lab Stranding Network. 
                     She is a recent graduate with a major in Marine Biology at UCSC. She began volunteering for the Long Marine Lab 
                     stranding network in 2017 as a first year and got hired as a Stranding Technician in 2021. During her time as an 
                     undergrad she participated in research at UCSC, Moss Landing, and The Marine Mammal Center. She has plans to continue 
                     research on marine mammals in graduate school. '
-                        photo={avatar} />
+                        photo={maiaImg} />
                 </div>
                 <div class="row">
-                    <Sponsor name='Samantha Rozal' bio=''
+                    <Researcher name='Samantha Rozal' bio=''
                         photo={avatar} />
                 </div>
             </div>
