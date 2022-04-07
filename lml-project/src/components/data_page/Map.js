4cd3b125010ec209b1066e36250e1991ce68576e
kaitlyn-liao <59896595+kaitlyn-liao@users.noreply.github.com>
2022-04-06 21:43:37 -0700

    api token connected and map rendered

--- lml-project/src/components/data_page/Map.js
+++ lml-project/src/components/data_page/Map.js
@@ -5,18 +5,62 @@
 
 // Map.js is rendered by DataVis.js, and currently renders no children.
 
-import React from 'react';
+// import React, { useRef, useEffect, useState } from 'react';
+// import ReactMapGL from "react-map-gl";
 
-class Map extends React.Component {
-    
-    render() {     
-      return (
-        // html goes here
-        <div>
+// class Map extends React.Component {
+//   render() {
+//     return (
+//       // html goes here
+//       <div>
+        
+//       </div>
+//     );
 
-        </div>
-      );
-    }
+//   }
+// }
+
+// export default Map
+
+import React, { useRef, useEffect, useState } from 'react';
+import MapGL, { Source, Layer, NavigationControl } from "@urbica/react-map-gl";
+import { withSize } from "react-sizeme";
+
+function Map(props) {
+
+  // Default map orientation
+  const [viewport, setViewport] = useState({
+    latitude: 36.954117,
+    longitude: -122.030799,
+    zoom: 9,
+  });
+
+  const SizeAware = withSize({ noPlaceholder: true, monitorHeight: true })(
+    (props) => props.children
+  );
+  const mapContainer = {
+    width: "100%", height: "83vh",
+  }
+  const mapRef = useRef();
+  // Resize the map to the current webpage size
+  const resizeMap = () => {
+    mapRef.current && mapRef.current.getMap().resize();
+  };
+
+  return (
+    <div>
+      <SizeAware onSize={resizeMap}>
+        <MapGL
+        {...viewport}
+        ref={mapRef}
+        style={ mapContainer}
+        accessToken={ process.env.REACT_APP_MAPBOX_TOKEN }
+        >
+          hi there 
+        </MapGL>
+      </SizeAware>
+    </div>
+  );
 }
 
-export default Map
+export default Map;
