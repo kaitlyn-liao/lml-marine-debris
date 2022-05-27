105565b4ee747b87a1f1a6d319d80beeabdb0644
Spencer Fulgham <spencerman@comcast.net>
2022-05-26 19:38:55 -0700

    small css changes

--- lml-project/src/components/Footer.js
+++ lml-project/src/components/Footer.js
@@ -16,17 +16,24 @@ function Footer() {
               <li>702 College Nine Road</li>
             </ui>
           </div>
-          {/* Column2 */}
           <div className="col">
-            <h4>Social Media</h4>
+            <h4>Visit Us!</h4>
             <ui className="list-unstyled">
-              <li>Twitter</li>
-              <li>Facebook</li>
-              <li>Instagram</li>
+              <a href="https://lmlstrandingnetwork.ucsc.edu/" target="_blank">Visit the offical LML website!</a>
+            </ui>
+            <ui className="list-unstyled">
+              <a href="https://lmlstrandingmap.herokuapp.com/" target="_blank">Visit our sister Marine Stranding website!</a>
             </ui>
           </div>
-          {/* Column3 */}
+          {/* Column2 */}
           <div className="col">
+            {/* <h4>Social Media</h4> */}
+            <ui className="list-unstyled">
+              © 2010-2022 Long Marine Lab Stranding Network | All Rights Reserved
+            </ui>
+          </div>
+        {/* Column3 */}
+        {/* <div className="col">
           <h4>Subscribe to our newsletter!</h4>
           <ui className="list-unstyled">
             <form>
@@ -34,13 +41,12 @@ function Footer() {
               <input type="submit" value="Submit" />
             </form>
           </ui>
-        </div>        
+        </div>         */}
         </div>
         <hr />
         <div className="row">
           <p className="col-sm">
-            &copy;{new Date().getFullYear()} UCSC LML Marine Debris | All rights reserved |
-            Terms Of Service | Privacy
+            &copy;{new Date().getFullYear()} UCSC Long Marine Lab Marine Debris Viewpoint
           </p>
         </div>
       </div>
