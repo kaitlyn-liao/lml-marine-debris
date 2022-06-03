2af99be34a35e4d3d35a84ddfc30790f28411914
kaitlyn-liao <59896595+kaitlyn-liao@users.noreply.github.com>
2022-06-03 13:00:41 -0700

    add axis label

--- lml-project/src/components/charts/BarBeachDebris.js
+++ lml-project/src/components/charts/BarBeachDebris.js
@@ -245,6 +245,14 @@ function BarChart() {
       }]
     },
     options: {
+      scales: {
+        y: {
+          title: {
+            display: true,
+            text: "Debris Pieces"
+          }
+        }
+      },
       plugins: {
         legend: {
           display: false
